4d411e9b7e57bd84061c3e500523c26ce2b20e61
검성\twili <jyjh4837@gmail.com>
2026-06-26 11:40:58 +0900

    완료

---
+++ Plugins/Temporary/Source/Temporary/Temporary.cpp
@@ -0,0 +1,14 @@
+#include "Temporary.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_MODULE(FTemporaryModule, Temporary)
+
+void FTemporaryModule::StartupModule()
+{
+    UE_LOG(LogTemp, Warning, TEXT("Temporary Module Startup"));
+}
+
+void FTemporaryModule::ShutdownModule()
+{
+    UE_LOG(LogTemp, Warning, TEXT("Temporary Module Shutdown"));
+}
